4dd1bfba92fd435d559ed34c07835fac9b8de23e
IvannaSW <ivanna.sav08@gmail.com>
2021-01-16 21:44:45 +0200

    Use Formik and Yup for Sign Up component

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -3,85 +3,93 @@ import "./sign-up.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
 
-class SignUp extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      displayName: "",
-      email: "",
+const SignUp = () => {
+  const formik = useFormik({
+    initialValues: {
+      displayName: '',
+      email: '',
       password: "",
       confirmPassword: "",
-    };
-  }
-  handleSubmit = async (event) => {
-    event.preventDefault();
-    const { displayName, email, password, confirmPassword } = this.state;
-    if (password !== confirmPassword) {
-      alert("Passwords don't match");
-      return;
-    }
-    try {
-      const { user } =  await auth.createUserWithEmailAndPassword(email, password);
-      await createUserProfileDocument(user, { displayName });      
-      this.setState({
-        displayName: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-      });      
-    } catch (error) {
-      console.error(error);
-    }    
-  };
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
-  render() {
-    const { displayName, email, password, confirmPassword } = this.state;
-    return (
-      <div className="sign-up">
+    },
+    validationSchema: Yup.object({
+      displayName: Yup.string()
+        .max(30, 'Must be 30 characters or less')
+        .required('Required'),      
+      email: Yup.string().email('Invalid email address').required('Required'),
+      password: Yup.string().required('Password is required'),
+      confirmPassword: Yup.string().required('Confirm Password is required')
+      .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    }),
+    onSubmit: async(values) => {
+      const { displayName, email, password } = values;     
+      try {
+        const { user } =  await auth.createUserWithEmailAndPassword(email, password);
+        await createUserProfileDocument(user, { displayName });     
+            
+      } catch (error) {
+        console.error(error);
+      }    
+    },
+  });
+  return(
+    <div className="sign-up">
         <h2 className="title">I don't have an account</h2>
         <span>Sign Up with your email and password</span>
-        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+        <form className="sign-up-form" onSubmit={formik.handleSubmit}>
           <FormInput
             type="text"
             name="displayName"
-            value={displayName}
-            onChange={this.handleChange}
-            label="Your Name"
-            required
+            id="displayName"
+            value={formik.values.displayName}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            label="Your Name"            
           />
+          {formik.touched.displayName && formik.errors.displayName ? (
+         <div className="form-error-message">{formik.errors.displayName}</div>
+       ) : null}
           <FormInput
             type="email"
+            id="email"
             name="email"
-            value={email}
-            onChange={this.handleChange}
-            label="Email"
-            required
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            label="Email"            
           />
+           {formik.touched.email && formik.errors.email ? (
+         <div className="form-error-message">{formik.errors.email}</div>
+       ) : null}
           <FormInput
             type="password"
             name="password"
-            value={password}
-            onChange={this.handleChange}
-            label="Password"
-            required
+            id="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            label="Password"            
           />
+           {formik.touched.password && formik.errors.password ? (
+         <div className="form-error-message">{formik.errors.password}</div>
+       ) : null}
           <FormInput
             type="password"
+            id="confirmPassword"
             name="confirmPassword"
-            value={confirmPassword}
-            onChange={this.handleChange}
-            label="Confirm Password"
-            required
+            value={formik.values.confirmPassword}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            label="Confirm Password"           
           />
+           {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+         <div className="form-error-message">{formik.errors.confirmPassword}</div>
+       ) : null}
           <CustomButton type="submit">SIGN UP</CustomButton>
         </form>
       </div>
-    );
-  }
+  )
 }
-
 export default SignUp;
