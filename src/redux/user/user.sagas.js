5f07cef727a5fc687fa046d5b588f2057c8e23d6
IvannaSW <ivanna.sav08@gmail.com>
2021-02-14 17:46:52 +0200

    Implement sign in with email and password using redux saga

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -5,7 +5,13 @@ import {
   googleProvider,
   createUserProfileDocument,
 } from "../../firebase/firebase.utils";
-import { googleSignInSuccess, googleSignInFailure } from "./user.actions";
+import {
+  googleSignInSuccess,
+  googleSignInFailure,
+  emailSignInSuccess,
+  emailSignInFailure,
+  emailSignInStart,
+} from "./user.actions";
 
 export function* signInWithGoogle() {
   try {
@@ -20,10 +26,27 @@ export function* signInWithGoogle() {
   }
 }
 
+export function* signInWithEmail({ payload: { email, password } }) {
+  try {
+    const { user } = yield auth.signInWithEmailAndPassword(email, password);
+    const userRef = yield call(createUserProfileDocument, user);
+    const userSnapshot = yield userRef.get();
+    yield put(
+      emailSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data })
+    );
+  } catch (error) {
+    yield put(emailSignInFailure);
+  }
+}
+
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
 
+export function* onEmailSignInStart() {
+  yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail);
+}
+
 export function* userSagas() {
-  yield all([call(onGoogleSignInStart)]);
+  yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
 }
