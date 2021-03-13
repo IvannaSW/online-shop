2530771a3b217d6b79ca7c5fa0f879ba681aef27
IvannaSW <ivanna.sav08@gmail.com>
2021-03-13 14:12:07 +0200

    Test context using for collection display

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './collection.styles.scss';
 import CollectionItem from '../../components/collection-item/collection-item.component';
 import {connect} from 'react-redux';
 import {selectCollection} from '../../redux/shop/shop.selectors';
+import CollectionsContext from '../../contexts/collection.context';
 
-const CollectionPage = ({collection}) => {
+
+const CollectionPage = ({match}) => {
+    const collections = useContext(CollectionsContext);
+    const collection = collections[match.params.collectionId];
     const { title, items } = collection;
     return (
         <div className='collection-page'>
@@ -18,8 +22,12 @@ const CollectionPage = ({collection}) => {
     )
 }
 
+export default CollectionPage;
+
+/*
 const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
 })
 
 export default connect(mapStateToProps)(CollectionPage);
+*/
